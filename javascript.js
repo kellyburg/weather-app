4f5f47f2150e91e4247f4f4ffd6125c756fe5e64
Kelly Burgess <Kelly@Guests-MacBook-Air.local>
2018-02-12 10:39:06 -0700

    location and api call

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
+var api = "https://fcc-weather-api.glitch.me/api/current?";
+var lat, lon;
+
 function getLocation(){
 if(navigator.geolocation){
    navigator.geolocation.getCurrentPosition(showPosition);
+   
 }
 else{
     document.getElementById("userLocation").innerHTML = "Unable to retrieve location";
 }
+
 }
 function showPosition(position) {
-    document.getElementById("userLocation").innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-}
+    var lat= position.coords.latitude;
+    var lon= position.coords.longitude;
+    console.log(lat);
+    console.log(lon);
+    var apiUrl = api + "lon=" + parseFloat(lon) + "&lat=" + parseFloat(lat);
+    console.log(apiUrl);
+ $.getJSON(apiUrl, function(json){
+    console.log(json);
+ })}
+
+function getWeather(){
+   }
